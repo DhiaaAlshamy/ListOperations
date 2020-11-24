7cf85d0ee7c723b1562a21bab08db6235634084c
Dhiaa Faisal Alshamy <dhodhof@gmail.com>
2020-11-24 20:19:58 +0000

    list length

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 ## Nested List #
 def nestedList():
+  print('#### Nested List ####')
   nested=['a',['b',['c'],['d','e'],'f'],'g']
   print('This id a nested list : ',nested)
   print('Example for accessing elements we use nested[1][2][0] :',nested[1][2][0])
+  print('#####################')
+  return nested;
+## list length ##
+def getLength(li):
+  print('#### list length ####')
+  print('The length of the passed list ', li,' is : ',len(li))
+  print('#####################')
 
-nestedList()
+
+getLength(nestedList())
