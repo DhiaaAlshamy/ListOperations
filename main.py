2539a214ce193b18773cee567d53c72a97b83e36
Dhiaa Faisal Alshamy <dhodhof@gmail.com>
2020-11-24 20:14:00 +0000

    add nested list method

---
+++ main.py
@@ -0,0 +1,7 @@
+## Nested List #
+def nestedList():
+  nested=['a',['b',['c'],['d','e'],'f'],'g']
+  print('This id a nested list : ',nested)
+  print('Example for accessing elements we use nested[1][2][0] :',nested[1][2][0])
+
+nestedList()
